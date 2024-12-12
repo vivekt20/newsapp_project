9e6ceff572d07ae141f49f07ab3d333fc534b43f
vivekt20 <vivekcherupoika20@gmail.com>
2024-12-12 07:44:37 +0530

    project

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:newsapp_project/article_detail.dart';
 import 'package:newsapp_project/newsmodel_api.dart';
 import 'model.dart';
-import 'article_detail.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,7 +13,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<List<NewsApiModel>> futureNews;
-  String selectedCategory = 'business'; // Default category
+  String selectedCategory = 'business'; 
+  TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
 
   @override
   void initState() {
@@ -28,35 +30,77 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void _updateSearchQuery(String query) {
+    setState(() {
+      _searchQuery = query;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('News Headlines'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: NewsSearchDelegate(futureNews),
-              );
-            },
-          ),
-        ],
+        backgroundColor: Colors.blue,
+        title: Center(child: const Text('NEWS HEADLINES',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildCategoryButton('Business', Icons.business, 'business'),
-                _buildCategoryButton(
-                    'Entertainment', Icons.movie, 'entertainment'),
-                _buildCategoryButton('Sports', Icons.sports, 'sports'),
-              ],
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+  children: [
+    GestureDetector(
+      onTap: () => _updateCategory('bussines'),
+      child: Text("BUSSINES",
+          style: TextStyle(
+            fontSize: 15,
+            color: selectedCategory == 'bussines' ? Colors.blue : Colors.black,
+            fontWeight: FontWeight.bold,
+          )),
+    ),
+    GestureDetector(
+      onTap: () => _updateCategory('entertainment'),
+      child: Text("ENTERTAINMENT",
+          style: TextStyle(
+            fontSize: 15,
+            color: selectedCategory == 'entertainment' ? Colors.blue : Colors.black,
+            fontWeight: FontWeight.bold,
+          )),
+    ),
+    GestureDetector(
+      onTap: () => _updateCategory('sports'),
+      child: Text("SPORTS",
+          style: TextStyle(
+            fontSize: 15,
+            color: selectedCategory == 'sports' ? Colors.blue : Colors.black,
+            fontWeight: FontWeight.bold,
+          )),
+    ),
+  ],
+),
+
+          ),
+          
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+            
+              controller: _searchController,
+              onChanged: _updateSearchQuery, 
+              decoration: InputDecoration(
+                labelText: 'Search News',
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    showSearch(
+                      context: context,
+                      delegate: NewsSearchDelegate(futureNews, query: _searchQuery),
+                    );
+                  },
+                ),
+              ),
             ),
           ),
           Expanded(
@@ -70,33 +114,52 @@ class _HomeScreenState extends State<HomeScreen> {
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
                   return const Center(child: Text('No articles found.'));
                 } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      final article = snapshot.data![index];
-                      return Card(
-                        margin: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          leading: article.urlToImage.isNotEmpty
-                              ? Image.network(article.urlToImage,
-                                  width: 50, height: 50, fit: BoxFit.cover)
-                              : const Icon(Icons.image_not_supported),
-                          title: Text(article.title,
-                              maxLines: 2, overflow: TextOverflow.ellipsis),
-                          subtitle: Text(article.description,
-                              maxLines: 2, overflow: TextOverflow.ellipsis),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    ArticleDetailPage(article: article),
-                              ),
-                            );
-                          },
+                  List<NewsApiModel> articles = snapshot.data!;
+
+                  
+                  if (_searchQuery.isNotEmpty) {
+                    articles = articles.where((article) {
+                      return article.title.toLowerCase().contains(_searchQuery.toLowerCase()) ||
+                          article.description.toLowerCase().contains(_searchQuery.toLowerCase());
+                    }).toList();
+                  }
+
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: articles.map((article) {
+                        return GestureDetector(
+                            onTap: (){
+                                Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ArticleDetailPage(article: article),
+                      ),
+                    );
+                            },
+                            child: Container(
+                            margin: EdgeInsets.all(15),
+                            child: Column(
+                                children: [
+                                    ClipRRect(
+                                        borderRadius: BorderRadius.circular(15),
+                                      child: Image.network(article.urlToImage,
+                                      height: 250,
+                                      width: 400,
+                                      fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10,),
+                                    Text(article.title,style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),),
+                                    Divider(thickness: 2,),
+                                ],
+                            ),
                         ),
-                      );
-                    },
+                        );
+                      }
+                      
+                      ).toList(),
+                    ),
                   );
                 }
               },
@@ -126,8 +189,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class NewsSearchDelegate extends SearchDelegate {
   final Future<List<NewsApiModel>> futureNews;
+  final String query;
 
-  NewsSearchDelegate(this.futureNews);
+  NewsSearchDelegate(this.futureNews, {required this.query});
 
   @override
   List<Widget>? buildActions(BuildContext context) {
